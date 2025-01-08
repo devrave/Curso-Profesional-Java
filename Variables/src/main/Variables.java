b834c86726c91fe61d02da15f0d9c1fea00280eb
andres1988 <a.canorave@gmail.com>
2025-01-08 13:41:07 -0500

    Starting profesional java course

---
+++ Variables/src/main/Variables.java
@@ -0,0 +1,67 @@
+package main;
+
+
+import java.awt.desktop.SystemEventListener;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        /*
+        * 4 tipos de datos
+        *
+        * Enteros
+        * Decimales
+        * Caracteres
+        * Booleanos
+        * */
+
+        /*int version;
+        version  = 5;
+        version  = 10;
+        System.out.println(version);
+
+        float nuevaVersionJava = 5.7f;
+        int version1 = 4;
+        char simbolo = 'l';
+        char numero = 4;
+        boolean valor = true;
+        boolean valor2 = false;
+        int String = 7;
+        System.out.println(String);
+        System.out.println(numero);
+
+        System.out.println(version1);
+        System.out.println(nuevaVersionJava);
+
+
+        byte diaMes = 127; // de -128 a 127
+        System.out.println(diaMes);
+
+        String string = "Como estas hermano";
+        System.out.println(string);*/
+
+
+        System.out.print("Hola,\nagregamos un salto");
+        System.out.print("\tagregamos un tab");
+        System.out.println(" ");
+
+        System.out.println("Mensaje con \\ diagonal inversa");
+        System.out.println("Mensaje con \"hola\" comillas dobles");
+        System.out.println("Mensaje con  'hola' comillas simples");
+
+
+        // CONSTANTES
+
+        final float pi;
+        pi = 3.1416f;
+
+
+        System.out.println(pi);
+
+
+
+
+
+
+    }
+}
---
+++ operadoresAritmeticos/src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        //operadores Aritméticos
+
+        int variableX = 50, variableY = 10;
+        int resultado;
+
+        resultado = variableX + variableY;
+        System.out.println(resultado);
+
+
+        resultado = variableX - variableY;
+        System.out.println(resultado);
+
+        resultado = variableX * variableY;
+        System.out.println(resultado);
+
+        resultado = variableX / variableY;
+        System.out.println(resultado);
+    }
+}
